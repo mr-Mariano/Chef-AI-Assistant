7fcbaad88c4a35ed4cdd87fd354a75b094ea63a9
Mariano <Mariano_romoo@hotmail.com>
2025-06-17 17:26:01 -0600

    Updated Markdown

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+// Al inicio de Recipe.tsx
 
 type Props = {
     recipe : string
@@ -7,22 +8,22 @@ type Props = {
 
 
 const markdownComponents = {
-    h2: ({node, ...props}) => (
+    h2: (props: any) => (
         <h2 className="scroll-m-20 border-b pb-2 text-2xl md:text-3xl font-semibold tracking-tight first:mt-0" {...props} />
     ),
-    p: ({node, ...props}) => (
+    p: (props: any) => (
         <p className="leading-7 [&:not(:first-child)]:mt-6" {...props} />
     ),
-    h4: ({node, ...props}) => (
+    h4: (props: any) => (
         <h4 className="scroll-m-20 text-md md:text-xl font-semibold tracking-tight mt-4" {...props} />
     ),
-    ul: ({ordered, node, ...props}) => (
+    ul: (props: any) => (
         <ul className="text-xs md:text-md my-6 ml-6 list-disc [&>li]:mt-2" {...props} />
     ),
-    ol: ({ordered, node, ...props}) => (
+    ol: (props: any) => (
         <ol className="text-xs md:text-md my-6 ml-6 list-disc [&>li]:mt-2" {...props} />
     ),
-    li: ({node, ordered, ...props}) => (
+    li: (props: any) => (
         <li className="mt-2" {...props} />
     ),
 };
